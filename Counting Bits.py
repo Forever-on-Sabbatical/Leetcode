dfa9baf91ad4012419805e3976d27ae006526599
BigBoyNoob <cheenu492001@gmail.com>
2022-05-15 18:07:25 +0530

    This is Day 1

--- Counting Bits.py
+++ Counting Bits.py
@@ -1,3 +1,5 @@
+#Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
 class Solution:
     def countBits(self, num: int) -> List[int]:
         counter = [0]
